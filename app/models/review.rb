56e57dc1e372f9c9a0bb15ffe3cd9ed7a5953c9a
Xiao Yu Zhu <cj_zhuyu@yahoo.com>
2015-03-08 00:14:23 -0500

    style error message

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :restaurant
+
+	validates :rating,  numericality: { only_integer: true,
+		greater_than_or_equal_to: 1,
+		less_than_or_equal_to: 5,
+		message: "Only whole number between 1 and 5 is acceptable for rating"
+	}
+	
+	validates :comment, presence: true
 end
