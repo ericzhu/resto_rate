c8a7b09eae5edddd4269114c8c393d0025e2d3a6
Xiao Yu Zhu <cj_zhuyu@yahoo.com>
2015-03-08 20:22:24 -0400

    Add validation on restaurant creation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,20 @@
 class Restaurant < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 	has_many :reviews, dependent: :destroy
+
+	    # t.string :name
+     #  t.string :address
+     #  t.string :phone
+     #  t.string :website
+     validates :name, :address, :phone, :website, presence: true
+     validates :phone, format: {
+     		with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+     		message: 'Phone number shall be in the format (123) 456-7890'
+     }
+
+     validates :website, format: {
+     		with:/\A^https?:\/\/.*\z/,
+     		message: 'Must start with http:// or https://'
+     }
+
 end
